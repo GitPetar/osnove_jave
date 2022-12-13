52eeb38b1af7270557a09924a13453b4011ef643
Petar Kotevski <petarkotevski123@gmail.com>
2022-12-13 20:20:43 +0100

    Za Vladu i/ili Milana, vidite Main

---
+++ src/main/java/p13_12_2022/Uvod.java
@@ -0,0 +1,51 @@
+package p13_12_2022;
+
+public class Uvod {
+
+    //Kad sam vodio DnD, hteo sam za igrace da napravim kod koji ce automatski da rolluje statove
+    public static void main(String[] args) {
+
+        //Prvo sam napravio varijable za statove
+        int strength = 0;
+        int dexterity = 0;
+        int constitution = 0;
+        int intelligence = 0;
+        int wisdom = 0;
+        int charisma = 0;
+        // zatim placeholder varijablu
+        double a;
+
+        //Petlja koja ce da rolluje statove dok se uslov ne ispuni
+        for (int i = 0; i < 10; ) {
+            a = Math.random() * 19;
+            strength = (int) a;
+            a = Math.random() * 19;
+            dexterity = (int) a;
+
+            a = Math.random() * 19;
+            constitution = (int) a;
+
+            a = Math.random() * 19;
+            intelligence = (int) a;
+            a = Math.random() * 19;
+            wisdom = (int) a;
+            a = Math.random() * 19;
+            charisma = (int) a;
+            a = strength + dexterity + constitution + intelligence + wisdom + charisma;
+        //Zelim da likovi budu izbalansirani, zato ce zbir svih statova da bude tacno 75
+            if (a == 75) {
+                break;
+
+            }
+        }
+        //Eureka, zadnji korak!
+        System.out.println("STR: " + strength);
+        System.out.println("DEX: " + dexterity);
+        System.out.println("CON: " + constitution);
+        System.out.println("INT: " + intelligence);
+        System.out.println("WIS: " + wisdom);
+        System.out.println("CHA: " + charisma);
+
+
+    }
+}
